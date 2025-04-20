c89a937c3bd475cee62101dfe99338790baab1bf
Adson Ventura <adsonventura@gmail.com>
2025-04-20 09:54:08 -0300

    adiciona either para validacao de erros e ajusta criacao de links para uso do mesmo

--- server/src/app/functions/create-link.ts
+++ server/src/app/functions/create-link.ts
@@ -1,6 +1,9 @@
 import { db } from "@/infra/db";
 import { schema } from "@/infra/db/schemas";
+import { makeLeft, makeRight, type Either } from "@/infra/shared/either";
+import { eq } from "drizzle-orm";
 import { z } from "zod";
+import { UrlAlreadySavedError } from "./errors/url-already-saved-error";
 
 const createLinkInput = z.object({
   originalLink: z.string().url(),
@@ -9,11 +12,38 @@ const createLinkInput = z.object({
 
 type CreateLinkInput = z.input<typeof createLinkInput>;
 
-export async function createLink(input: CreateLinkInput) {
+export async function createLink(input: CreateLinkInput): Promise<
+  Either<
+    UrlAlreadySavedError,
+    {
+      id: string;
+      originalUrl: string;
+      shortUrl: string;
+      createdAt: Date;
+      totalClicks: number;
+    }
+  >
+> {
   const { originalLink, shortLink } = createLinkInput.parse(input);
 
-  await db.insert(schema.links).values({
-    originalUrl: originalLink,
-    shortUrl: shortLink,
-  });
+  const isAlreadyUsed = await db
+    .select({
+      shortLink: schema.links.shortUrl,
+    })
+    .from(schema.links)
+    .where(eq(schema.links.shortUrl, shortLink));
+
+  if (isAlreadyUsed[0]) {
+    return makeLeft(new UrlAlreadySavedError("A url encurtada já existe"));
+  }
+
+  const result = await db
+    .insert(schema.links)
+    .values({
+      originalUrl: originalLink,
+      shortUrl: shortLink,
+    })
+    .returning();
+
+  return makeRight(result[0]);
 }
--- server/src/infra/http/routes/create-new-link.ts
+++ server/src/infra/http/routes/create-new-link.ts
@@ -1,4 +1,5 @@
 import { createLink } from "@/app/functions/create-link";
+import { isRight, unwrapEither } from "@/infra/shared/either";
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 
@@ -15,15 +16,29 @@ export const createNewLinkRoute: FastifyPluginAsyncZod = async (server) => {
         }),
         response: {
           201: z.null().describe("Link created"),
+          400: z.object({
+            message: z.string(),
+          }),
         },
       },
     },
     async (request, reply) => {
       const { originalLink, shortLink } = request.body;
-      // console.log(teste);
-      await createLink({ originalLink, shortLink });
 
-      return reply.status(201).send();
+      const result = await createLink({ originalLink, shortLink });
+
+      if (isRight(result)) {
+        return reply.status(201).send();
+      }
+
+      const error = unwrapEither(result);
+
+      switch (error.constructor.name) {
+        case "UrlAlreadySavedError":
+          return reply
+            .status(400)
+            .send({ message: "A URL encurtada já existe" });
+      }
     }
   );
 };
